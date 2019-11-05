c90afd163602ad496c73ef99daf70239961148a6
Igor Melo <igorjmelo4@gmail.com>
2019-11-05 13:12:33 -0300

    adding domains to server

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
 const { ApolloServer } = require("apollo-server");
+const dns = require('dns');
 
 const typeDefs = `
   type Item {
     id: Int
     type: String
     description: String
+  }
+
+  type Domain {
+    name: String
+    checkout: String
+    available: Boolean
   }
 
   type Query {
@@ -19,6 +26,7 @@ const typeDefs = `
   type Mutation {
     saveItem (item: ItemInput): Item
     deleteItem (id: Int): Boolean
+    gerenateDomains: [Domain]
   }
 
 `;
@@ -31,6 +39,18 @@ const items = [
   { id: 5, type: "sufix", description: "Station"},
   { id: 6, type: "sufix", description: "Mart"},
 ];
+
+const isDomainAvailable = function(url) {
+  return new Promise(function(resolve, reject) {
+    dns.resolve(url, function(err) {
+      if(error) {
+        resolve(true)
+      } else {
+        resolve(false);
+      }
+    });
+  });
+};
 
 const resolvers = {
   Query: {
@@ -54,6 +74,23 @@ const resolvers = {
       item.splice(items.indexOf(item), 1);
 
       return true
+    },
+    async gerenateDomains() {
+      const domains = [];
+			for (const prefix of items.filter(item => item.type === "prefix")) {
+				for (const sufix of items.filter(item => item.type === "sufix")) {
+					const name = prefix.description + sufix.description;
+					const url = name.toLowerCase();
+          const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+          const available = await isDomainAvailable(`${url}.com.br`);
+					domains.push({
+						name,
+            checkout,
+            available
+					});
+				}
+      }
+      return domains;
     }
   }
 };
